49a746ede6e84ba84331438f2300e66209c7d710
srivastavaharshit2217-beep <srivastavaharshit2217@gmail.com>
2026-09-02 11:00:51 +0530

    Fix vertical page scrolling

---
+++ scroll-fix.js
@@ -0,0 +1,4 @@
+// Keep normal vertical page scrolling after the premium homepage banner is applied.
+document.documentElement.style.overflowY = 'auto';
+document.body.style.overflowX = 'hidden';
+document.body.style.overflowY = 'auto';
